3cf8a9ee94e1837b263a5d44969a8422257c8efb
murasame29 <77433104+murasame29@users.noreply.github.com>
2023-06-25 00:59:00 +0900

    Update Quiz.go

--- api/Quiz.go
+++ api/Quiz.go
@@ -8,19 +8,19 @@ import (
 )
 
 type getQuizRequest struct {
-	hits int64 `form:"hits" binding:"min=1,max=10"`
+	Hits int64 `form:"hits" binding:"min=1,max=10"`
 }
 
 func (server *Server) getQuiz(ctx *gin.Context) {
 	var req getQuizRequest
 	if err := ctx.ShouldBindQuery(&req); err != nil {
-		log.Println(req.hits)
+		log.Println(req.Hits)
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
-	if req.hits == 0 {
-		req.hits++
+	if req.Hits == 0 {
+		req.Hits++
 	}
 
 	data, err := server.conn.GetQuiz(req.hits)
